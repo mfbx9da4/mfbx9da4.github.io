b793285af6a2f910d11f3c5097286d653e42d785
David Adler <dalberto.adler@gmail.com>
2014-02-14 10:46:43 -0200

    refactored globals into peices object

--- main.js
+++ main.js
@@ -31,24 +31,70 @@ var INIT_SQUARES = [
 
 // -----Checkers and sqaures config-----
 // 1 red, 2 black
-var CHECKER_NAMES = {1: 'RED', 2: 'BLACK'};
-var CHECKER_COLORS = {1: 'firebrick', 2: '#313131'};
-var CHECKER_BORDER = {width: 5, color: '#000'};
+// var CHECKER_NAMES = {1: 'RED', 2: 'BLACK'};
+// var CHECKER_COLORS = {1: 'firebrick', 2: '#313131'};
+// var CHECKER_BORDER = {width: 5, color: '#000'};
 // the int representing the checker of the other color
-var ANTI_CHECKER = {1: 2, 2: 1};
+// var ANTI_CHECKER = {1: 2, 2: 1};
 // 1 can move bottom right or bottom left. (second value in array need to check if checer can jump)
 // 2 can move top right or top left relative to self.
-var CHECKER_MOVES = {1: [{row: 1, col: 1}, {row: 1, col: -1}], 
-                     2: [{row: -1, col: 1}, {row: -1, col: -1}]};
-var SCORES = {1: 0, 2: 0};
+
 // 0 black, 1 white, 2 active
 var SQUARE_COLORS = {0: '#ccc', 1: '#fff', 2: 'steelblue'};
+
+var teams = {
+    1: {name: 'RED', 
+        score: 0,
+        color: 'firebrick'
+    }, 
+    2: {name: 'BLACK', 
+        score: 0,
+        color: '#313131'
+    }
+};
+
+var queens = [3, 4];
+
+// checker peices
+var peices = {
+    1: {name: 'red pawn',
+        color: 'firebrick',
+        moves: [{row: 1, col: 1}, {row: 1, col: -1}],
+        border: {width: 5, color: '#000'},
+        anti: 2,
+        team: 1
+    },
+    2: {name: 'black pawn',
+        color: '#313131',
+        moves: [{row: -1, col: 1}, {row: -1, col: -1}],
+        border: {width: 5, color: '#000'},
+        anti: 1,
+        team: 2
+    },
+    3: {name: 'red queen',
+        color: 'firebrick',
+        moves: [{row: 1, col: 1}, {row: 1, col: -1}, 
+                {row: -1, col: 1}, {row: -1, col: -1}],
+        border: {width: 5, color: 'gold'},
+        anti: 2,
+        team: 1
+    },
+    2: {name: 'black queen',
+        color: '#313131',
+        moves: [{row: 1, col: 1}, {row: 1, col: -1}, 
+                {row: -1, col: 1}, {row: -1, col: -1}],
+        border: {width: 5, color: 'gold'},
+        anti: 1,
+        team: 2
+    }
+};
 
 // -----Dimensions config------
 var SQUARE_SIZE = 50;
 var BOARD_DIM = INIT_SQUARES.length;
 var BOARD_WIDTH = SQUARE_SIZE * BOARD_DIM;
 var BOARD_HEIGHT = SQUARE_SIZE * BOARD_DIM;
+var INFO_WIDTH = SQUARE_SIZE * 2;
 
 function deep_copy(array) {
     var out = Array(array.length);
@@ -77,48 +123,49 @@ function Board (canvas) {
 
 Board.prototype.find_legal_moves = function(pos) {
     var board = this;
-    var checker = board.checkers[pos.row][pos.col];
-    // anti means a checker of opposite color
-    var anti = ANTI_CHECKER[checker];
+    if (pos.row >=0 && pos.row < BOARD_DIM) {
+        var checker = board.checkers[pos.row][pos.col];
+        // anti means a checker of opposite color
 
-    var moves = CHECKER_MOVES[checker];
+        if (peices[checker] !== undefined && checker === board.who_to_play) {
+            // if user selected a checker and it is this color to play 
+            var anti = peices[checker].anti;
+            var moves = peices[checker].moves;
+            for (var i = 0; i < moves.length; i++) {
+                var delta = moves[i];
 
-    if (moves !== undefined && checker === board.who_to_play) {
-        // if it is this color to play
-        for (var i = 0; i < moves.length; i++) {
-            var delta = moves[i];
+                // new trial position
+                var mv = {};
+                mv.row = pos.row + delta.row;
+                mv.col = pos.col + delta.col;
 
-            // new trial position
-            var mv = {};
-            mv.row = pos.row + delta.row;
-            mv.col = pos.col + delta.col;
-
-            // jump pos
-            var jump ={};
-            jump.row = pos.row + (delta.row * 2);
-            jump.col = pos.col + (delta.col * 2);
-            
-            if (mv.row >= 0 && mv.row < BOARD_DIM && 
-                board.checkers[mv.row][mv.col] === 0) {
-                // adjacent empty
-                board.animated.push(mv);
-            } else if (mv.row < BOARD_DIM && jump.row < BOARD_DIM &&
-                       mv.row >= 0 && jump.row >= 0 &&
-                       board.checkers[mv.row][mv.col] === anti &&
-                       board.checkers[jump.row][jump.col] === 0) {
+                // jump pos
+                var jump ={};
+                jump.row = pos.row + (delta.row * 2);
+                jump.col = pos.col + (delta.col * 2);
                 
-                // can jump adjacent
-                board.animated.push({row: jump.row, col: jump.col});
+                if (mv.row >= 0 && mv.row < BOARD_DIM && 
+                    board.checkers[mv.row][mv.col] === 0) {
+                    // adjacent empty
+                    board.animated.push(mv);
+                } else if (mv.row < BOARD_DIM && jump.row < BOARD_DIM &&
+                           mv.row >= 0 && jump.row >= 0 &&
+                           board.checkers[mv.row][mv.col] === anti &&
+                           board.checkers[jump.row][jump.col] === 0) {
+                    
+                    // can jump adjacent
+                    board.animated.push({row: jump.row, col: jump.col});
+                }
+
+
             }
-
-
-        }
-        if (board.animated.length) {
-            // also animate square beneath checker if it has legal moves
-            board.animated.push(pos);
-            board.selected_checker = pos;
-            board.selected_checker.color = checker;
-            return board.animated;
+            if (board.animated.length) {
+                // also animate square beneath checker if it has legal moves
+                board.animated.push(pos);
+                board.selected_checker = pos;
+                board.selected_checker.team = peices[checker].team;
+                return board.animated;
+            }
         }
     }
     return [];
@@ -128,8 +175,8 @@ Board.prototype.find_legal_moves = function(pos) {
 Board.prototype.find_jump_moves = function(pos) {
     var board = this;
     var checker = board.checkers[pos.row][pos.col];
-    var anti = ANTI_CHECKER[checker];
-    var moves = CHECKER_MOVES[checker];
+    var anti = peices[checker].anti;
+    var moves = peices[checker].moves;
 
     if (moves !== undefined && checker === board.who_to_play) {
         // if it is this color to play
@@ -158,7 +205,7 @@ Board.prototype.find_jump_moves = function(pos) {
         if (board.animated.length) {
             // board.animated.push(pos);
             board.selected_checker = pos;
-            board.selected_checker.color = checker;
+            board.selected_checker.team = checker;
             return board.animated;
         }
     }
@@ -227,11 +274,11 @@ Board.prototype.selectSquare = function(pos) {
 
 Board.prototype.move = function(pos) {
     var board = this;
-
+    var switch_player;
     // Source and destination positions
     var src = board.selected_checker;
     var des = pos;
-    
+    // make the move
     board.checkers[des.row][des.col] = board.checkers[src.row][src.col];
     board.checkers[src.row][src.col] = 0;
 
@@ -244,7 +291,7 @@ Board.prototype.move = function(pos) {
         inter.row = src.row + (delta.row / 2);
         inter.col = src.col + (delta.col / 2);
 
-        SCORES[board.selected_checker.color] ++;
+        teams[board.selected_checker.team].score ++;
         board.updateScore();
         board.checkers[inter.row][inter.col] = 0;
         board.reset_animated();
@@ -254,28 +301,55 @@ Board.prototype.move = function(pos) {
             board.must_jump = true;
             return board.show_legal();
         } else {
-            board.switch_player();
+            // can not do second jump
+            switch_player = true;
         }
     } else {
-        board.switch_player();
+        // did not jump
+        switch_player = true;
     }
+
+    // check if has become a queen
+    for (var i = 0; i < queens.length; i++) {
+        var queen = peices[queens[i]];
+        if (des.row === queen.trigger_row && src.team === queen.team) {
+            // make queen
+            board.checkers[des.row][des.col] = queens[i];
+            switch_player = false;
+        }
+    }
+    // if (des.row === BOARD_DIM-1 && src.team === 1) {
+    //     // make red queen
+    //     board.checkers[des.row][des.col] = 3;
+    //     switch_player = false;
+    // } else if (des.row === 0 && src.team === 2) {
+    //     // make black queen
+    //     board.checkers[des.row][des.col] = 4;
+    //     switch_player = false;
+
+    }
+    // reset_animated, switch_player = false
+    // check team and switch to queen black or red
+    // if des == BOARD_DIM, change_peice, 
+    // find_queen_legal_moves(pos), 
+
+    if (switch_player) {board.switch_player();}
     board.reset_animated();
-    board.draw();
 };
 
 Board.prototype.updateScore = function() {
     var board = this;
-    for (var i in SCORES) {
-        document.querySelector('#score' + i).innerText = SCORES[i];
+    for (var i in teams) {
+        document.querySelector('#score' + i).innerText = teams[i].score;
     }
 };
 
 Board.prototype.switch_player = function() {
     var board = this;
-    board.who_to_play = ANTI_CHECKER[board.who_to_play];
-    var name = CHECKER_NAMES[board.who_to_play];
+    board.who_to_play = peices[board.who_to_play].anti;
+    var name = teams[board.who_to_play].name;
     document.querySelector('#whos-turn-to-play b').innerHTML = name;
-    var color = CHECKER_COLORS[board.who_to_play];
+    var color = peices[board.who_to_play].color;
     document.getElementById('whos-turn-to-play').style.backgroundColor = color;
 };
 
@@ -329,7 +403,7 @@ Board.prototype.draw_board = function() {
 
 Board.prototype.draw_checkers = function() {
     var board = this;
-    var radius = (SQUARE_SIZE / 2) - CHECKER_BORDER.width;
+    var radius = (SQUARE_SIZE / 2) - peices[1].border.width;
 
     for (var row = 0; row < BOARD_DIM; row++) {
         for (var col = 0; col < BOARD_DIM; col++) {
@@ -345,10 +419,10 @@ Board.prototype.draw_checkers = function() {
 
                 board.ctx.beginPath();
                 board.ctx.arc(center_x, center_y, radius, 0, 2 * Math.PI, false);
-                board.ctx.fillStyle = CHECKER_COLORS[checker];
+                board.ctx.fillStyle = peices[checker].color;
                 board.ctx.fill();
-                board.ctx.lineWidth = CHECKER_BORDER.width;
-                board.ctx.strokeStyle = CHECKER_BORDER.color;
+                board.ctx.lineWidth = peices[checker].border.width;
+                board.ctx.strokeStyle = peices[checker].border.color;
                 board.ctx.stroke();
             }
         }
@@ -356,7 +430,16 @@ Board.prototype.draw_checkers = function() {
 };
 
 function initCanvas() {
+    // container
     var container = document.getElementById('checkers-game-container');
+    container.style.width = (BOARD_WIDTH + INFO_WIDTH + 4) + 'px';
+    container.style.height = BOARD_HEIGHT + 'px';
+    container.style.margin = '30px auto';
+    container.style.boxShadow = '0px 0px 30px 2px #888';
+    container.style.border = '1px solid #888';
+    container.style.backgroundColor = 'rgb(153, 153, 153)';
+
+    // canvas
     var canvas = document.createElement('canvas');
     canvas.width = BOARD_WIDTH;
     canvas.height = BOARD_HEIGHT;
@@ -367,6 +450,8 @@ function initCanvas() {
 
 function initInfoDiv() {
     var container = document.getElementById('checkers-game-container');
+
+    // info div
     var info_div = document.createElement('div');
     info_div.id = 'checkers-game-info-div';
     info_div.style.fontFamily = 'Arial';
@@ -391,7 +476,7 @@ function initInfoDiv() {
     var score = document.createElement('div');
 
     var score1 = document.createElement('div');
-    score1.style.backgroundColor = CHECKER_COLORS[1];
+    score1.style.backgroundColor = peices[1].color;
     score1.style.padding = '8px 8px';
     score1.style.margin = '70px 10px';
     score1.style.borderRadius = "4px";
@@ -401,7 +486,7 @@ function initInfoDiv() {
     score.appendChild(score1);
 
     var score2 = document.createElement('div');
-    score2.style.backgroundColor = CHECKER_COLORS[2];
+    score2.style.backgroundColor = peices[2].color;
     score2.style.padding = '8px 8px';
     score2.style.margin = '70px 10px';
     score2.style.borderRadius = "4px";
