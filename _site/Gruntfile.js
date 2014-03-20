2c39b19ef05e2afa2da5ab748074f424571a892d
David Adler <dalberto.adler@gmail.com>
2014-03-20 09:36:45 -0300

    changed href

--- _site/Gruntfile.js
+++ _site/Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         src: {
             html: [
+                '_layouts/jade/*.jade',
                 '_layouts/*.html',
                 'index.html'
             ],
