a695bfb0a280ec7cdc84eb4399062f339b88d8bc
David Adler <dalberto.adler@gmail.com>
2015-03-23 16:27:20 -0300

    added slideout to projects
    Former-commit-id: 0837f9807d5b413c5f23554de63485d18b7d53e2

--- _site/Gruntfile.js
+++ _site/Gruntfile.js
@@ -81,6 +81,7 @@ module.exports = function(grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['jade', 'jekyll:build', 'concurrent:target']);
+    grunt.registerTask('watch', ['watch']);
 
 
     // Load the plugin that provides the "uglify" task.
