f829154efd8e3f0b215b893e03198137ab20d506
David Adler <dalberto.adler@gmail.com>
2016-03-14 20:18:03 -0300

    added click me hover message
    Former-commit-id: 8f59f0481c42f796f9291a09223d6031363e6660

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	console.log($('h1.title'));
+	$('h1.title').hover(function() {
+		$('.profile-img').addClass('hover');
+	});
+	$('h1.title').mouseleave(function() {
+		console.log('leave hover');
+		$('.profile-img').removeClass('hover');
+	});
+
+	$('.profile-img').hover(function() {
+		$('.hover-message').show();
+		$('.hover-message').addClass('bounceInRight');
+	})
+
+	$('.profile-img').mouseleave(function() {
+		$('.hover-message').hide();
+		$('.hover-message').removeClass('bounceInRight');
+	})
+});
