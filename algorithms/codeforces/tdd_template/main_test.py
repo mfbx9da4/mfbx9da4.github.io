5747f6fa4694bc79e883d8d54472a1b4d0233331
David Adler <dalberto.adler@gmail.com>
2020-04-22 11:13:15 +0100

    added tdd env

---
+++ algorithms/codeforces/tdd_template/main_test.py
@@ -0,0 +1,26 @@
+import unittest
+from .main import solve
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_solve(self):
+        self.assertEqual(solve([2, 1]), 3)
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
